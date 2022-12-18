0128e90ebbde4467d9147d1ed7c67371e5c54569
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-12-18 15:40:23 +0700

    register aman

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $registrationData = $request->all(); //mengambil seluruh data input dan menyimpan dalam variabel registrationData
+
+        $validate = Validator::make($registrationData, [
+            'name' => 'required|max:60',
+            'email' => 'required|email:rfc,dns|unique:users',
+            'password' => 'required|min:8',
+//            'password' => 'required|min:8|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+//            'image' => 'required|file'
+
+        ]); //rule validasi input saat register
+
+//        $path = $request->file('image')->store('users','public');
+//        $registrationData['image']=basename($path);
+        if($validate->fails()) //mengecek apakah inputan sudah sesuai dengan rule validasi
+            return response(['message' => $validate->errors()], 400); //mengembalikan error validasi input
+
+        $registrationData['password'] = bcrypt($request->password);
+
+        $user = User::create($registrationData); //membuat user baru
+        $user->sendEmailVerificationNotification();
+
+        return response([
+            'status' => true,
+            'message' => 'Register Success',
+            'user' => $user,
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ], 200); //return data user dalam bentuk json
+    }
+
+    public function login(Request $request){
+        $loginData = $request->all();
+
+        $validate = Validator::make($loginData, [
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required|min:8',
+        ]);
+
+        if($validate->fails())
+            return response(['message' => $validate->errors()], 400);
+
+        if(!Auth::attempt($loginData))
+            return response(['message' => 'Invalid Credentials'], 401); //mengembalikan error gagal login
+
+//        $user = Auth::user();
+//        $token = $user->createToken('Authentication Token')->accessToken; //generate token
+        $user = User::where('email', $request->email)->first();
+        if (!$user->hasVerifiedEmail()) {
+            return response()->json([
+                'status' => false,
+                "message" => "Email isn't' verified"
+            ], 400);
+        }
+        return response([
+            'status' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ]); //return data user dan token dalam bentuk json
+
+    }
+
+    public function logout(Request $request){
+        try{
+            $request->user()->token()->revoke();
+            return response([
+                'message' => 'Logout Successfully!!'
+            ]);
+        }catch (\Exception $e){
+            return response([
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserVerificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('email/verify/{id}', [UserVerificationController::class, 'verify'])->name('verification.verify');
+Route::get('email/resend', [UserVerificationController::class, 'resend'])->name('verification.resend');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
